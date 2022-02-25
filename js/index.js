269ce89e0b286f7a656a559235b291fb3f52e2bb
Cissy <91531100+cissyverhaeghe@users.noreply.github.com>
2022-02-25 11:34:20 +0100

    tiles with new colors are added when clicked

--- js/index.js
+++ js/index.js
@@ -1,14 +1,46 @@
-function getRandomColor(){
-    
+//generate 3 random numbers between 0 and 255
+
+function getRandomColor() {
+  let randomNumbers = [];
+  let numbers = 3;
+  while (numbers--) {
+    randomNumbers.push(Math.floor(Math.random() * 256));
+  }
+  return `rgb(${randomNumbers[0]}, ${randomNumbers[1]}, ${randomNumbers[2]})`;
 }
 
+class Tile {
+  constructor(holder, color) {
+    this.holder = holder;
+    this.color = color;
+    this.htmlRef = this.generateInitialHTML();
+    this.setUpEvents();
+    this.setBackgroundColor();
+  }
 
-class Tile{
-    constructor(bg){
-        this.bg = getRandomColor();
-        this.generateInitialHTML();
-        this.setStyling();
-    }
+  generateInitialHTML() {
+    this.holder.insertAdjacentHTML("beforeend", `<div class="tile"></div>`);
+    return this.holder.querySelector(`.tile:last-child`);
+  }
 
+  setBackgroundColor() {
+    this.htmlRef.style.background = this.color;
+  }
 
+  setStyling() {
+    this.htmlRef.style.height = "80%";
+  }
+
+  setUpEvents() {
+    this.htmlRef.onclick = () => {
+      this.setStyling();
+      new Tile(holder, getRandomColor());
+    };
+  }
 }
+
+const holder = document.querySelector("body");
+const color = getRandomColor();
+
+const tile = new Tile(holder, color);
+console.log(tile.bg);
